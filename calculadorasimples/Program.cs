982fd2b608bfd5bac9f8b5c9abd1061187dce74c
matheus <matheus.f.silva31@edu.senai.br>
2026-04-15 14:35:54 -0300

    feat: adicionado suporte as quatro operacoes e tratamento de erro

--- calculadorasimples/Program.cs
+++ calculadorasimples/Program.cs
@@ -1,28 +1,41 @@
 ﻿using System;
 
-namespace calculadorasimples
+namespace CalculadoraSimples
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-----calculadora SENAI----");
-
-            Console.Write("Digite o primeiro numero: ");
+            Console.WriteLine("--- Calculadora Avançada SENAI ---");
+           
+            Console.Write("Digite o primeiro número: ");
             double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Digite o segundo numero: ");
+            Console.Write("Digite o segundo número: ");
             double num2 = Convert.ToDouble(Console.ReadLine());
 
-            double soma = num1 + num2;
-            Console.WriteLine($"O Resultado de soma é {soma}");
+            Console.WriteLine("Escolha a operação: (+, -, *, /)");
+            string operacao = Console.ReadLine();
 
-
-            
-
+            double resultado = 0;
 
             
+            switch (operacao)
+            {
+                case "+": resultado = num1 + num2; break;
+                case "-": resultado = num1 - num2; break;
+                case "*": resultado = num1 * num2; break;
+                case "/":
+                    
+                    resultado = num2 != 0 ? num1 / num2 : 0;
+                    if(num2 == 0) Console.WriteLine("Erro: Divisão por zero!");
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida!");
+                    return;
+            }
 
+            Console.WriteLine($"O resultado de {num1} {operacao} {num2} é: {resultado}");
         }
     }
 }
